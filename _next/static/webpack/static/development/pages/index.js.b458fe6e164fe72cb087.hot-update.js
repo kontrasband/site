5f8904bf8294a09770e8b561ca3af8589f541c05
Daniël Botha <botha.daniel1@gmail.com>
2019-05-09 19:44:20 +0200

    Move static files

---
+++ _next/static/webpack/static/development/pages/index.js.b458fe6e164fe72cb087.hot-update.js
@@ -0,0 +1,79 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/About.jsx":
+/*!******************************!*\
+  !*** ./components/About.jsx ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return About; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _UpcomingEvents__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./UpcomingEvents */ "./components/UpcomingEvents.jsx");
+
+
+function About() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    id: "about",
+    className: "about overlay main"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "background-img"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: "static/img/about-bg.jpf",
+    alt: ""
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "container"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "row  vertical-align"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "col-lg-5 col-md-12"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "front-p"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "uppercase text-white"
+  }, "A different ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), "kind of music"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: " w-93"
+  }, "We are Kontras \u2013 a Stellenbosch-based alternative Afrikaans rock band. We are a newly formed band with a signature blend of rock, punk and alternative soundscapes. Our first EP, Side A, is a collection of tracks that encapsulate the band\u2019s contrasting sound as well as our dynamic range in subject matter. ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), "Kontras consists of four university friends who met at Stellenbosch University due to their shared musical experiences; Jacques Groenewald on vocals, Andrew Veldman on lead guitar, Louwrens Labuschagne on the bass guitar and Dani\xEBl Botha driving their sound forward on drums. Growing up listening to music that ranges from an eclectic mix of punk, pop, metal, and hip-hop, Kontras doesn\u2019t define themselves as a singular concept, but as a collection of the shared experiences of young adults who feel frustrated with the status quo."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+    className: "block-social list-inline mb-4 mb-lg-0"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    className: "list-inline-item mr-0"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "https://itunes.apple.com/za/artist/kontras/1445772282"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+    className: "socicon-apple"
+  }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    className: "list-inline-item mr-0"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "https://play.google.com/store/music/artist/Kontras?id=Aanobdoml5f2absa7i3ooltrm3q&hl=en"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+    className: "socicon-play"
+  }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    className: "list-inline-item mr-0"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "https://www.amazon.com/gp/product/B07KY4GB56/ref=dm_ws_sp_ps_dp"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+    className: "socicon-amazon"
+  }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    className: "list-inline-item mr-0"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "https://open.spotify.com/artist/13mo5g6PR09u3Rq8bEstY2?si=sytJqeERQw2EI8u1mZn0UQ"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+    className: "socicon-spotify"
+  }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    className: "list-inline-item mr-0"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "https://www.youtube.com/channel/UC3PTVGTBJz9UmWwOxhS1i9g"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+    className: "socicon-youtube"
+  })))))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "col-lg-7"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_UpcomingEvents__WEBPACK_IMPORTED_MODULE_1__["default"], null)))));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.b458fe6e164fe72cb087.hot-update.js.map
