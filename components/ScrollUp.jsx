bf0b5b829d0df29268092b3b34d7af8febab3e24
Daniël Botha <botha.daniel1@gmail.com>
2019-05-09 19:43:49 +0200

    Build

--- components/ScrollUp.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ScrollUp() {
-  return (
-    <a className="block-top scroll" href="#wrapper">
-      <i className="icon-angle-up" />
-    </a>
-  );
-}
