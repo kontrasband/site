024755ced45ed2b9b1177c8eaa016efff874ca01
Daniël Botha <daniel@polymorph.co.za>
2020-03-20 19:02:24 +0200

    Updates

---
+++ precache-manifest.ad1bde31cf4694c2a91a735185376c39.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "21f68fe93474db3144b9",
+    "url": "/static/js/main.68401809.chunk.js"
+  },
+  {
+    "revision": "d0ac75bb044d5920fcb9",
+    "url": "/static/js/runtime~main.e3174e2c.js"
+  },
+  {
+    "revision": "8dd606b354b3235e6d50",
+    "url": "/static/js/2.d14cdc9c.chunk.js"
+  },
+  {
+    "revision": "074f8bf2d25a33b273ff",
+    "url": "/static/js/3.e67f057f.chunk.js"
+  },
+  {
+    "revision": "e79d7db258b3a1ff6e42",
+    "url": "/static/js/4.f9f33355.chunk.js"
+  },
+  {
+    "revision": "7e28bea250f924676b09",
+    "url": "/static/js/5.c4a5fe2a.chunk.js"
+  },
+  {
+    "revision": "d82da603f80d359a44e1",
+    "url": "/static/js/6.4dbba2d4.chunk.js"
+  },
+  {
+    "revision": "1d5ff72b668ebbbf4978",
+    "url": "/static/js/7.659b7f79.chunk.js"
+  },
+  {
+    "revision": "873a581eb923c246af4a",
+    "url": "/static/js/8.c4e2a299.chunk.js"
+  },
+  {
+    "revision": "03509882b59b4a350ff7f9f9acac879e",
+    "url": "/index.html"
+  }
+];
