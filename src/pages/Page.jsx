98efec6224a00987fff7bd62a8e6219bca5e4a97
Daniël Botha <daniel@polymorph.co.za>
2020-04-05 10:52:12 +0200

    Add Contact component to bottom of all pages

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Contact from '../components/Contact';
 
 export default function Page({ name, children }) {
   return (
     <section id={name} className="main page-container">
       {children}
+      <Contact />
     </section>
   )
 }
