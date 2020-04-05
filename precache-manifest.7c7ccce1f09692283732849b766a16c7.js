67175e980a40d37dc0aedfbed3075253d75e6d99
Daniël Botha <daniel@polymorph.co.za>
2020-04-05 10:52:48 +0200

    Updates

---
+++ precache-manifest.7c7ccce1f09692283732849b766a16c7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "157f680cb6fc9a00626d",
+    "url": "/static/js/main.92d72abf.chunk.js"
+  },
+  {
+    "revision": "006bde823eccd5befb2a",
+    "url": "/static/js/runtime~main.759b3507.js"
+  },
+  {
+    "revision": "a6f1e79c7af7bc4b1ab4",
+    "url": "/static/js/2.953e6e85.chunk.js"
+  },
+  {
+    "revision": "d20cf5cc7da60fb4708b",
+    "url": "/static/js/3.617466c3.chunk.js"
+  },
+  {
+    "revision": "7e21cbd53b790abce003",
+    "url": "/static/js/4.5a8b7796.chunk.js"
+  },
+  {
+    "revision": "17e57f3886dfe0fb5d55",
+    "url": "/static/js/5.1e87e20f.chunk.js"
+  },
+  {
+    "revision": "36b8815788f710ab6fb5",
+    "url": "/static/js/6.86333954.chunk.js"
+  },
+  {
+    "revision": "0c0241b5db1fa7dad2c0",
+    "url": "/static/js/7.02b84047.chunk.js"
+  },
+  {
+    "revision": "a45400d314fb0ec5911a",
+    "url": "/static/js/8.76fe4f04.chunk.js"
+  },
+  {
+    "revision": "0ca9c10c32576fb496ba",
+    "url": "/static/js/9.50de6a4b.chunk.js"
+  },
+  {
+    "revision": "818ec31ff2624a5915f6",
+    "url": "/static/js/10.850ed100.chunk.js"
+  },
+  {
+    "revision": "e34bdf166846d34f4c0d",
+    "url": "/static/js/11.5745e436.chunk.js"
+  },
+  {
+    "revision": "9d28ea469a29d47a4ed4e5d23f223a6a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cb0cede78170e8d502b3cdc3a737f3f.js"
+  "/precache-manifest.7c7ccce1f09692283732849b766a16c7.js"
 );
 
 workbox.clientsClaim();
