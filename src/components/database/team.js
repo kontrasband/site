b3728b7bb2709fee1217a829fb88367fbfbdf907
Daniël Botha <daniel@polymorph.co.za>
2021-04-15 11:34:39 +0200

    Remove Ian from team

--- src/components/database/team.js
+++ src/components/database/team.js
@@ -6,13 +6,13 @@ export default [
     insta: "disarnold",
     img: "/static/img/team/arnold.jpg",
     about: [
-      { key: "hometown", value: "Mossel Bay", },
-      { key: "profession", value: "Radio Programme Manager", },
-      { key: "Top Intl Artist", value: "Nothing But Thieves", },
-      { key: "Top Local Artist", value: "Foto Na Dans", },
-      { key: "Top Album", value: "ODESZA - A Moment Apart (Deluxe)", },
-      { key: "quote", value: "\"Read. Understand. Reduce the stupid.\"" },
-    ]
+      { key: "hometown", value: "Mossel Bay" },
+      { key: "profession", value: "Radio Programme Manager" },
+      { key: "Top Intl Artist", value: "Nothing But Thieves" },
+      { key: "Top Local Artist", value: "Foto Na Dans" },
+      { key: "Top Album", value: "ODESZA - A Moment Apart (Deluxe)" },
+      { key: "quote", value: '"Read. Understand. Reduce the stupid."' },
+    ],
   },
   {
     name: "Justin Reinecke",
@@ -21,13 +21,13 @@ export default [
     insta: "justin_reinecke_adv",
     img: "/static/img/team/justin.jpg",
     about: [
-      { key: "hometown", value: "Durbanville", },
-      { key: "profession", value: "Filmmaker/photographer", },
-      { key: "Top Intl Artist", value: "Kaleo", },
-      { key: "Top Local Artist", value: "Spoegwolf", },
-      { key: "Top Album", value: "Spoegwolf - Koma", },
-      { key: "quote", value: "\"Don't take shit too seriously\"" },
-    ]
+      { key: "hometown", value: "Durbanville" },
+      { key: "profession", value: "Filmmaker/photographer" },
+      { key: "Top Intl Artist", value: "Kaleo" },
+      { key: "Top Local Artist", value: "Spoegwolf" },
+      { key: "Top Album", value: "Spoegwolf - Koma" },
+      { key: "quote", value: '"Don\'t take shit too seriously"' },
+    ],
   },
   {
     name: "Jo Ellis",
@@ -36,42 +36,34 @@ export default [
     insta: "joellismusic",
     img: "/static/img/team/jo.jpg",
     about: [
-      { key: "hometown", value: "Ladismith", },
-      { key: "profession", value: "Record Producer", },
-      { key: "Top Intl Artist", value: "Pearl Jam", },
-      { key: "Top Local Artist", value: "Hugo Veldsman", },
-      { key: "Top Album", value: "Pearl Jam - Vs.", },
-      { key: "quote", value: "\"Moenie op 'n perd klim as jy nie OK is met afval nie.\"" },
-    ]
-  },
-  {
-    name: "Ian Neethling",
-    role: "Merch Guy",
-    align: "right",
-    insta: "neethling.io",
-    img: "/static/img/team/ian.jpg",
-    about: [
-      { key: "hometown", value: "Paarl", },
-      { key: "profession", value: "Software Engineer", },
-      { key: "Top Intl Artist", value: "John Mayer", },
-      { key: "Top Local Artist", value: "Hugo Veldsman & The Rendezvous", },
-      { key: "Top Album", value: "John Mayer - Where The Light Is Live at the Nokia Theatre", },
-      { key: "quote", value: "\"Worrying is like a rocking chair. It gives you something to do, but it doesn’t get you anywhere.\" - Van Wilder" },
-    ]
+      { key: "hometown", value: "Ladismith" },
+      { key: "profession", value: "Record Producer" },
+      { key: "Top Intl Artist", value: "Pearl Jam" },
+      { key: "Top Local Artist", value: "Hugo Veldsman" },
+      { key: "Top Album", value: "Pearl Jam - Vs." },
+      {
+        key: "quote",
+        value: '"Moenie op \'n perd klim as jy nie OK is met afval nie."',
+      },
+    ],
   },
   {
     name: "Nanna Venter",
     role: "Designer",
-    align: "left",
+    align: "right",
     insta: "nannaventer",
     img: "/static/img/team/nanna.jpg",
     about: [
-      { key: "hometown", value: "Bellville", },
-      { key: "profession", value: "Artstronaut", },
-      { key: "Top Intl Artist", value: "Johnny Foreigner", },
-      { key: "Top Local Artist", value: "Yndian Mynah", },
-      { key: "Top Album", value: "The Superweaks - The World Is A Terrible Place And I Hate Myself And I Want To Die", },
-      { key: "quote", value: "\"Never norm out!\"" },
-    ]
+      { key: "hometown", value: "Bellville" },
+      { key: "profession", value: "Artstronaut" },
+      { key: "Top Intl Artist", value: "Johnny Foreigner" },
+      { key: "Top Local Artist", value: "Yndian Mynah" },
+      {
+        key: "Top Album",
+        value:
+          "The Superweaks - The World Is A Terrible Place And I Hate Myself And I Want To Die",
+      },
+      { key: "quote", value: '"Never norm out!"' },
+    ],
   },
 ];
